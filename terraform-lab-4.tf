e26c6838cd8ee3822dd8e704a38a54320a88d8c0
ksarkar-devops <kaushik89sarkar@gmail.com>
2025-06-14 11:52:22 +0530

    allow_nested_items_to_be_public = "true" in lab 4

--- terraform-lab-4.tf
+++ terraform-lab-4.tf
@@ -29,6 +29,7 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  allow_nested_items_to_be_public = "true"
 
   tags = {
     Environment = "Terraform Getting Started"
