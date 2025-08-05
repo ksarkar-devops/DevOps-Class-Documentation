800d3ea639f9ed12a0510c2c8066ff62272fb03d
Kaushik Sarkar <kaushik89sarkar@gmail.com>
2025-08-05 22:14:45 +0530

    Update pipeline script for project 1.js

--- pipeline script for project 1.js
+++ pipeline script for project 1.js
@@ -4,33 +4,34 @@ pipeline {
     stages {
         stage('Clone repo') {
             steps {
-                echo 'Clone GitHub Repository'
 				git branch: 'main', url:"https://github.com/ksarkar-devops/spring-boot-hello-world.git"
             }
         }
 		stage('List files') {
             steps {
-                echo 'List all files in Jenkins Workspace folder'
-				sh 'cd /var/lib/jenkins/workspace/DeployJavaProject/'
-				sh 'ls -l'
+				sh '''
+				    cd /var/lib/jenkins/workspace/DeployJavaProject/
+				    ls -l
+			    '''
             }
         }
         stage('Project Build') {
             steps {
-                echo 'Build'
-				sh 'mvn clean package'
+                sh '''
+				    mvn clean package
+			    '''
             }
         }
 		stage('Docker build') {
             steps {
-                echo 'Docker build'
-                sh 'cd /var/lib/jenkins/workspace/DeployJavaProject/'
-                sh 'docker build -t java_proj .'
+                sh '''
+				    cd /var/lib/jenkins/workspace/DeployJavaProject/
+				    docker build -t java_proj .
+			    '''
             }
         }
-        stage('Docker Login') {
+        stage('ACR Login') {
             steps {
-                echo 'Docker Login'
 				withCredentials([usernamePassword(credentialsId: 'acr-login', usernameVariable: 'ACR_USER', passwordVariable: 'ACR_PASS')]) {
                     sh '''
                         echo "$ACR_PASS" | docker login ksarkar23987.azurecr.io --username "$ACR_USER" --password-stdin
@@ -40,9 +41,10 @@ pipeline {
         }
 		stage('ACR Push') {
             steps {
-                echo 'ACR Push'
-                sh 'docker tag java_proj ksarkar23987.azurecr.io/test-repo:test-tag'
-                sh 'docker push ksarkar23987.azurecr.io/test-repo:test-tag'
+                sh '''
+				    docker tag java_proj ksarkar23987.azurecr.io/test-repo:test-tag
+				    docker push ksarkar23987.azurecr.io/test-repo:test-tag
+			    '''
             }
         }
     }
