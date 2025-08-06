2a12d9cce6ce1821a8120128e2283fe1143e051d
Kaushik Sarkar <kaushik89sarkar@gmail.com>
2025-08-06 22:11:10 +0530

    Update pipeline script for project 1.groovy

--- pipeline script for project 1.groovy
+++ pipeline script for project 1.groovy
@@ -1,6 +1,13 @@
 pipeline {
     agent any
-
+    
+    environment {
+        IMAGE_NAME = 'test-repo'
+        IMAGE_TAG = 'test-tag'
+        CLUSTER_NAME = 'kube-cluster'
+        NAMESPACE = 'default'
+    }
+    
     stages {
         stage('Clone repo') {
             steps {
@@ -21,7 +28,7 @@ pipeline {
 		stage('Docker build') {
             steps {
                 sh 'cd /var/lib/jenkins/workspace/DeployJavaProject/'
-				sh 'docker build -t java_proj .'
+				sh 'docker build -t javaproj .'
             }
         }
         stage('ACR Login') {
@@ -35,6 +42,33 @@ pipeline {
             steps {
                 sh 'docker tag java_proj ksarkar23987.azurecr.io/test-repo:test-tag'
 				sh 'docker push ksarkar23987.azurecr.io/test-repo:test-tag'
+            }
+        }
+		stage('AKS Login') {
+            steps {
+                sh 'az login --identity'
+                sh 'az account set --subscription 719c1e53-b3fd-4af0-99f9-46469a324ca7'
+				sh 'az aks get-credentials --resource-group rg --name kube-cluster --overwrite-existing'
+            }
+        }
+		stage('Confirm after AKS Login') {
+            steps {
+                sh 'kubectl config current-context'
+				sh 'kubectl get nodes'
+            }
+        }
+        stage('Deploy to AKS') {
+            steps {
+                sh '''
+                    if kubectl get deployment ${IMAGE_NAME} -n ${NAMESPACE}; then
+                        echo "Deployment exists. Updating image..."
+                        kubectl set image deployment/${IMAGE_NAME} ${IMAGE_NAME}=${ACR_NAME}/${IMAGE_NAME}:${IMAGE_TAG} -n ${NAMESPACE}
+                    else
+                        echo "Creating new deployment..."
+                        kubectl create deployment ${IMAGE_NAME} --image=${ACR_NAME}/${IMAGE_NAME}:${IMAGE_TAG} -n ${NAMESPACE}
+                    fi
+                '''
+                sh 'kubectl rollout status deployment/${IMAGE_NAME} -n ${NAMESPACE}'
             }
         }
     }
