3f655afde3dcef887abccc3c0eebcd2c73bec0ac
Kaushik Sarkar <kaushik89sarkar@gmail.com>
2025-08-06 12:39:20 +0530

    Update pipeline script for project 1.js

--- pipeline script for project 1.js
+++ pipeline script for project 1.js
@@ -9,42 +9,32 @@ pipeline {
         }
 		stage('List files') {
             steps {
-				sh '''
-				    cd /var/lib/jenkins/workspace/DeployJavaProject/
-				    ls -l
-			    '''
+				sh 'cd /var/lib/jenkins/workspace/DeployJavaProject/'
+				sh 'ls -l'
             }
         }
         stage('Project Build') {
             steps {
-                sh '''
-				    mvn clean package
-			    '''
+                sh 'mvn clean package'
             }
         }
 		stage('Docker build') {
             steps {
-                sh '''
-				    cd /var/lib/jenkins/workspace/DeployJavaProject/
-				    docker build -t java_proj .
-			    '''
+                sh 'cd /var/lib/jenkins/workspace/DeployJavaProject/'
+				sh 'docker build -t java_proj .'
             }
         }
         stage('ACR Login') {
             steps {
 				withCredentials([usernamePassword(credentialsId: 'acr-login', usernameVariable: 'ACR_USER', passwordVariable: 'ACR_PASS')]) {
-                    sh '''
-                        echo "$ACR_PASS" | docker login ksarkar23987.azurecr.io --username "$ACR_USER" --password-stdin
-                    '''
+                    sh 'echo "$ACR_PASS" | docker login ksarkar23987.azurecr.io --username "$ACR_USER" --password-stdin'
                 }
             }
         }
 		stage('ACR Push') {
             steps {
-                sh '''
-				    docker tag java_proj ksarkar23987.azurecr.io/test-repo:test-tag
-				    docker push ksarkar23987.azurecr.io/test-repo:test-tag
-			    '''
+                sh 'docker tag java_proj ksarkar23987.azurecr.io/test-repo:test-tag'
+				sh 'docker push ksarkar23987.azurecr.io/test-repo:test-tag'
             }
         }
     }
