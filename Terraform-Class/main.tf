19a955d56142df6ad2ee1bdc2e91c8191c126cd7
Kaushik Sarkar <kaushik89sarkar@gmail.com>
2025-08-30 14:27:12 +0530

    Create main.tf

---
+++ Terraform-Class/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
+}
+
+# Create subnets using for_each loop
+resource "azurerm_subnet" "main" {
+  for_each = { for subnet in var.subnets : subnet.name => subnet }
+
+  name                 = each.value.name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [each.value.address_prefix]
+}
